2026db54ac4479c7bbc45212c98b3805b72f6950
Md. Mehedi Hasan Emon <60240261+Mehedi-bit@users.noreply.github.com>
2022-05-22 17:12:07 +0600

    token put and delete method successfully added

--- handlers/routeHandlers/tokenHandler.js
+++ handlers/routeHandlers/tokenHandler.js
@@ -80,7 +80,6 @@ handler._token.get = (requestProperties, callback) => {
         && requestProperties.queryStringObject.id.trim().length === 20
             ? requestProperties.queryStringObject.id
             : false;
-    console.log(requestProperties.queryStringObject.id, id);
     if (id) {
         // lookup the token
         data.read('tokens', id, (err, tokenData) => {
@@ -101,10 +100,70 @@ handler._token.get = (requestProperties, callback) => {
 };
 // @TODO: Authentication
 // for put
-handler._token.put = (requestProperties, callback) => {};
+handler._token.put = (requestProperties, callback) => {
+    const id = typeof requestProperties.body.id === 'string'
+    && requestProperties.body.id.trim().length === 20 ? requestProperties.body.id : false;
+
+    const extend = typeof requestProperties.body.extend === 'boolean'
+    && requestProperties.body.extend === true ? requestProperties.body.extend : false;
+
+    if (id && extend) {
+        data.read('tokens', id, (err1, tokenData) => {
+            // check if the token is already expired. if already expired we will not extend token expiry time
+            const tokenObject = parseJSON(tokenData);
+            if (tokenObject.expires > Date.now()) {
+                tokenObject.expires = Date.now() + 60 * 60 * 1000;
+
+                // store the updated token
+                data.update('tokens', id, tokenObject, (err2) => {
+                    if (!err2) {
+                        callback(200);
+                    } else {
+                        callback(500, {
+                            error: 'There was a server side error',
+                        });
+                    }
+                });
+            } else {
+                callback(400, {
+                    error: 'Token already expired',
+                });
+            }
+        });
+    } else {
+        callback(400, {
+            error: 'There was a problem in your request.',
+        });
+    }
+};
 // @TODO: Authentication
-// for delete
-handler._token.delete = (requestProperties, callback) => {};
+// for token delete - means user logout
+handler._token.delete = (requestProperties, callback) => {
+    // check the token if valid
+    const id = typeof requestProperties.queryStringObject.id === 'string'
+        && requestProperties.queryStringObject.id.trim().length === 20
+            ? requestProperties.queryStringObject.id
+            : false;
+
+    if (id) {
+        // lookup the token
+        data.read('tokens', id, (err1, tokenData) => {
+            if (!err1 && tokenData) {
+                data.delete('tokens', id, (err2) => {
+                    if (!err2) {
+                        callback(200, { message: 'Token was successfully deleted.' });
+                    } else {
+                        callback(500, { error: 'There was a server side error!' });
+                    }
+                });
+            } else {
+                callback(500, { error: 'There was a server side error!' });
+            }
+        });
+    } else {
+        callback(400, { error: 'There was a problem in your request!' });
+    }
+};
 
 // exporting
 module.exports = handler;
